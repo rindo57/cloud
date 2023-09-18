132ddf60b7eac0edbdf39271c04c4bfb8590a673
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-09-18 20:26:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ async def remote_upload(request):
 
 async def file_html(request):
     hash = request.match_info["hash"]
-    download_link = f"http://cloud.techzbots.live/dl/{hash}"
+    download_link = f"http://192.53.162.132/dl/{hash}"
     filename = is_hash_in_db(hash)["filename"]
 
     return web.Response(
