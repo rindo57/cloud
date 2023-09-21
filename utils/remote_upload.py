1374d1e63b9b6ffeabd5ec501f163829bcfe6c1e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-09-21 20:32:04 +0530

    Update remote_upload.py

--- utils/remote_upload.py
+++ utils/remote_upload.py
@@ -1,18 +1,8 @@
 from utils.download import DL_STATUS, download_file
 from utils.upload import upload_file_to_channel
-import re
-import requests
 
-def getFilename_fromCd(cd):
-    if not cd:
-        return None
-    fname = re.findall('filename=(.+)', cd)
-    if len(fname) == 0:
-        return None
-    return fname[0]
 async def start_remote_upload(session, hash, url):
     ext = await download_file(session, hash, url)
-    r = requests.get(url, allow_redirects=True)
-    orgname = getFilename_fromCd(r.headers.get('content-disposition'))
+    orgname = hash + "." + ext
     if ext:
-        await upload_file_to_channel(hash, hash + "." + ext, ext,orgname)
+        await upload_file_to_channel(hash, hash + "." + ext, ext, orgname)
