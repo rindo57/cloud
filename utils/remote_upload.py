5954ebee2aa23bbee25f35ab4c8d5956fe49ecea
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-09-21 20:20:17 +0530

    Update remote_upload.py

--- utils/remote_upload.py
+++ utils/remote_upload.py
@@ -7,8 +7,8 @@ def getFilename_fromCd(cd):
     if not cd:
         return None
         fname = re.findall('filename=(.+)', cd)
-    if len(fname) == 0:
-        return None
+        if len(fname) == 0:
+            return None
     return fname[0]
 async def start_remote_upload(session, hash, url):
     ext = await download_file(session, hash, url)
